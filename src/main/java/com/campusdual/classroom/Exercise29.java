497d48ee46f18a9b733f924cca72d51ad8b924f2
Sara <sarareyvalino23@gmail.com>
2024-09-27 09:58:41 +0200

    Corregido

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
 public class Exercise29 {
-    public static void main(String[] args) throws Exception{
-        throw new Exception("Testeando");
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException("This is a controlled exception.");
+        } catch (Exception e) {
+            System.err.println("Caught an exception: " + e.getMessage());
+        }
     }
 }
